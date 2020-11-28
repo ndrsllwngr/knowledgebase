e48dbdd0a26f8233c6500403047e777dca261c5d
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-11-28 20:24:56 +0100

    Add Style Guide

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    "Introduction": ['introduction/introduction'],
+    "Introduction": ['introduction/introduction', 'introduction/style_guide'],
     "Languages": ['languages/css', 'languages/haskell', 'languages/html', 'languages/java', 'languages/javascript', 'languages/prolog', 'languages/python', 'languages/rust', 'languages/scala', 'languages/typescript'],
     "Frameworks": ['frameworks/android_native', 'frameworks/next_js', 'frameworks/node_js', 'frameworks/vue_js'],
     "Libraries": ['libraries/react'],
