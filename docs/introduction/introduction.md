a1e101b1a5c5d677175bc600b4469d44c841a747
griase94 <andy.griesbeck@gmail.com>
2020-12-01 17:07:49 +0100

    Create archive for unused files & sort sidebar

--- docs/introduction/introduction.md
+++ docs/introduction/introduction.md
@@ -6,11 +6,11 @@ slug: /
 
 ## Languages
 - [CSS](../languages/css.md)
-- [Haskell](../languages/haskell.md)
+- [Haskell](docs/_archive/haskell.md)
 - [HTML](../languages/html.md)
 - [Java](../languages/java.md)
 - [JavaScript](../languages/javascript.md)
-- [Prolog](../languages/prolog.md)
+- [Prolog](docs/_archive/prolog.md)
 - [Python](../languages/python.md)
 - [Rust](../languages/rust.md)
 - [Scala](../languages/scala.md)
--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,10 @@
 module.exports = {
   someSidebar: {
-    "Introduction": ['introduction/introduction', 'introduction/style_guide'],
-    "Languages": ['languages/css', 'languages/haskell', 'languages/html', 'languages/java', 'languages/javascript', 'languages/prolog', 'languages/python', 'languages/rust', 'languages/scala', 'languages/typescript'],
-    "Frameworks": ['frameworks/android_native', 'frameworks/next_js', 'frameworks/node_js', 'frameworks/vue_js'],
-    "Libraries": ['libraries/react'],
-    "OS": ['os/linux', 'os/macos'],
-    "Applications": ['applications/git', 'applications/homebrew', 'applications/oh_my_zsh', 'applications/shell', 'applications/visual_studio_code']
+    "Introduction": ['introduction/style_guide','introduction/introduction'].sort(),
+    "Languages": ['languages/css', 'languages/html', 'languages/java', 'languages/javascript', 'languages/python', 'languages/rust', 'languages/scala', 'languages/typescript'].sort(),
+    "Frameworks": ['frameworks/android_native', 'frameworks/next_js', 'frameworks/node_js', 'frameworks/vue_js'].sort(),
+    "Libraries": ['libraries/react'].sort(),
+    "OS": ['os/linux', 'os/macos'].sort(),
+    "Applications": ['applications/git', 'applications/homebrew', 'applications/oh_my_zsh', 'applications/shell', 'applications/visual_studio_code'].sort()
   },
 };
