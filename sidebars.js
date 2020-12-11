dc864cde5da294ef62bd233138b27d80dfc7cb82
overdoz <thanh-le@hotmail.de>
2020-12-11 10:10:43 +0100

    find ip address - shell

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     "Introduction": ['introduction/style_guide','introduction/introduction'].sort(),
-    "Languages": ['languages/css', 'languages/html', 'languages/java', 'languages/javascript', 'languages/python', 'languages/rust', 'languages/scala', 'languages/typescript'].sort(),
+    "Languages": ['languages/go', 'languages/css', 'languages/html', 'languages/java', 'languages/javascript', 'languages/python', 'languages/rust', 'languages/scala', 'languages/typescript'].sort(),
     "Frameworks": ['frameworks/android_native', 'frameworks/next_js', 'frameworks/node_js', 'frameworks/vue_js'].sort(),
     "Libraries": ['libraries/react'].sort(),
     "OS": ['os/linux', 'os/macos'].sort(),
